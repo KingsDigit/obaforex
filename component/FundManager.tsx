b84c2c84e7f94a6dac147d8845c03381da23ea24
Kings Digital Services Ltd <48445746+KingsDigit@users.noreply.github.com>
2024-05-07 04:24:11 +0100

    Update FundManager.tsx

--- component/FundManager.tsx
+++ component/FundManager.tsx
@@ -11,7 +11,7 @@ export const FundManager = ({ bgColor }: prop) => {
       <div className="container">
         <div className="row">
           <div className="col-md-10 col-12">
-            <h3 className="bold">Let's Help You Manage Your Forex Account</h3>
+            <h3 className="bold">Let's Help You Manage Your Forex/Synthetic Account</h3>
             <p>
               We are top in the game of Forex. Let's help you manage your forex
               Account{" "}
